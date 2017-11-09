3cf56e8171f18e237b25137e6913a44c83c1db3f
SachokD-TC <Rythm1977>
2017-11-09 17:24:59 +0200

    initial commit

---
+++ android/assets/map/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="new" tilewidth="50" tileheight="32" tilecount="60" columns="5">
+ <image source="isometric_grass_and_water.png" width="256" height="384"/>
+</tileset>
